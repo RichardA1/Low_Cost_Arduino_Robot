81e6a4ee049311cb248b0143913ff2cf5a50cad9
Richard Albritton <richardalbritton@gmail.com>
2014-05-02 08:17:47 -0700

    Update Light_following_robot.ino

--- Light_following_robot/Light_following_robot.ino
+++ Light_following_robot/Light_following_robot.ino
@@ -1,30 +1,104 @@
-/*
- * Braitenberg vehicle
- * This code was adapted to work with the robot found at:
- * http://richardalbritton.wordpress.com/arduino-robot/
- * Original code is from http://tinkerlog.com/2009/04/18/arduino-powered-braitenberg-vehicle/
- */
 
-int right = 1; // For PWM
-int left = 4; // For PWM
-int R_photo = 2; // For Analog IN
-int L_photo = 3; // For Analog IN
-//int extra = 0; // For I2C or SPI Data IN
-int R_turn = 0;
-int L_turn = 0;
+int servoL = 0;
+int servoR = 4;
+const int lineTrigL = 1;   // pin#2 on the Trinket is Anolog Pin#1 and connected to the left line sensor.
+const int lineTrigR = 3;   // pin#3 on the Trinket is Anolog Pin#3 and connected to the right line sensor.
 
-// the setup routine runs once when you press reset:
-void setup() {
-// initialize the digital pin as an output.
-pinMode(right, OUTPUT);
-pinMode(left, OUTPUT);
+//For delayMicroseconds: Half way is 745. +745 gose CW, -745 gose CCW
+// 604 microseconds at 30 times = CW 360%
+// 876 microseconds at 30 times = CCW 360%
+const int turnCW = 604;
+const int turnCCW = 856;
+
+int lineReadL = 0;  // Reading of the left sensor
+int lineReadR = 0;  // Reading of the right sensor
+
+int lineMaxL = 300;//(analogRead(1) + 223);  // nuber that defines Black
+int lineMaxR = 300;//(analogRead(3) + 223);  // nuber that defines Black
+
+int var = 0;  // Used to loop the servo movements
+int randNumber = 0;  // Used to choose a direction to turn
+
+void setup()
+{
+ pinMode(servoL, OUTPUT);
+ pinMode(servoR, OUTPUT);
+ //pinMode(2, INPUT);
+ //pinMode(4, INPUT);
+  //valb = analogRead(2);    // read the input pin
 }
 
-void loop() {
-  // sensor values between 50..900
-  R_turn = (analogRead(R_photo) / 4);
-  L_turn = (analogRead(L_photo) / 4);
-  analogWrite(right, (220 - R_turn));
-  analogWrite(left, (220 - L_turn));
-  delay(10);
+void Go_forward(){ // Drive drive forward
+    // Start to turn the left wheel CCW.
+    digitalWrite(servoL, HIGH);
+    delayMicroseconds(turnCCW);
+    digitalWrite(servoL, LOW);
+    // Start to turn the right wheel CW.
+    digitalWrite(servoR, HIGH);
+    delayMicroseconds(turnCW);
+    digitalWrite(servoR, LOW);
+    delay(10);
 }
+void Go_backward(){ // Drive drive backward
+    // Start to turn the left wheel CW.
+    digitalWrite(servoL, HIGH);
+    delayMicroseconds(turnCW);
+    digitalWrite(servoL, LOW);
+    // Start to turn the right wheel CCW.
+    digitalWrite(servoR, HIGH);
+    delayMicroseconds(turnCCW);
+    digitalWrite(servoR, LOW);
+    delay(10);
+}
+void TurnL(){ // Turn Left
+    // Start to turn the left wheel CCW.
+    digitalWrite(servoL, HIGH);
+    delayMicroseconds(turnCCW);
+    digitalWrite(servoL, LOW);
+    // Start to turn the right wheel CCW.
+    digitalWrite(servoR, HIGH);
+    delayMicroseconds(turnCCW);
+    digitalWrite(servoR, LOW);
+    delay(100);
+}
+void TurnR(){ // Turn Right
+    // Start to turn the left wheel CW.
+    digitalWrite(servoL, HIGH);
+    delayMicroseconds(turnCW);
+    digitalWrite(servoL, LOW);
+    // Start to turn the right wheel CW.
+    digitalWrite(servoR, HIGH);
+    delayMicroseconds(turnCW);
+    digitalWrite(servoR, LOW);
+    delay(100);
+}
+
+void loop()
+{
+ lineReadL = analogRead(lineTrigL);  // Reading of the left sensor
+ lineReadR = analogRead(lineTrigR);  // Reading of the right sensor
+ 
+ if (lineReadR < lineMaxR && lineReadL < lineMaxL){
+   Go_forward();
+    randNumber = random(200);
+    //lineMaxL = (analogRead(lineTrigL) + 223);  // nuber that defines Black
+    //lineMaxR = (analogRead(lineTrigR) + 223);  // nuber that defines Black
+ }else if (lineReadL > lineMaxL && lineReadR > lineMaxR){
+   if (randNumber < 99) {
+     TurnL();
+     TurnL();
+    TurnL();
+    TurnL();
+   } else {
+     TurnR();
+     TurnR();
+     TurnR();
+     TurnR();
+   }
+  }else if (lineReadR > lineMaxR){
+   TurnL();
+ }else if (lineReadL > lineMaxL){
+   TurnR();
+ }
+}
+
